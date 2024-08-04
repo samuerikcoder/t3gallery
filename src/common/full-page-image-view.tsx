04022ff2f1a2defd0c257811a6ac59d3f07173a6
samuerikcoder <eriksamumedeiros@gmail.com>
2024-08-04 20:26:46 -0300

    remove unecessary code

--- src/common/full-page-image-view.tsx
+++ src/common/full-page-image-view.tsx
@@ -7,14 +7,7 @@ export async function FullPageImageView(props: { photoId: string }) {
   const idAsNumber = Number(props.photoId);
   if (Number.isNaN(idAsNumber)) throw new Error("Invalid photo id");
 
-  const image = await getImage(idAsNumber).then((image) => {
-    try {
-      if (!image) redirect("/");
-      return image;
-    } catch (error) {
-      redirect("/");
-    }
-  });
+  const image = await getImage(idAsNumber);
 
   const userInfo = await clerkClient.users.getUser(image.userId);
 
